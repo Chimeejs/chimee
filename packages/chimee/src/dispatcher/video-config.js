6b36bb1e8b52340ffa4b540e3dc2dcbbd68d93c4
toxic-johann <353904974@qq.com>
2017-07-23 22:59:10 +0800

    [bugfix] solve singleton video-config bug what: why: how:

--- packages/chimee/src/dispatcher/video-config.js
+++ packages/chimee/src/dispatcher/video-config.js
@@ -24,7 +24,7 @@ function accessorVideoProperty (property: string): Function {
   });
 }
 
-function accessorVideoAttribute (attribute: string | {set: string, get: string, isBoolean: boolean}): Function {
+function accessorVideoAttribute (attribute: string | {set: string, get: string, isBoolean?: boolean}): Function {
   const {set, get, isBoolean} = isObject(attribute)
     ? attribute
     : {
@@ -51,12 +51,14 @@ function accessorVideoAttribute (attribute: string | {set: string, get: string, 
   });
 }
 
-function accessorCustomAttribute (attribute: string, isBoolean: boolean): Function {
+function accessorCustomAttribute (attribute: string, isBoolean?: boolean): Function {
   return accessor({
     get (value) {
       const attrValue = this.dispatcher.dom.getAttr('video', attribute);
       return (this.dispatcher.videoConfigReady && this.inited)
-        ? attrValue
+        ? isBoolean
+          ? !!attrValue
+          : attrValue
         : value;
     },
     set (value) {
@@ -68,19 +70,200 @@ function accessorCustomAttribute (attribute: string, isBoolean: boolean): Functi
   });
 }
 
+const accessorMap = {
+  src: [
+    alwaysString(),
+    accessor({
+      set (val: string) {
+        if(this.needToLoadSrc) {
+          // unlock it at first, to avoid deadlock
+          this.needToLoadSrc = false;
+          this.dispatcher.bus.emit('load', val);
+        }
+        return val;
+      }
+    }, {preSet: false}),
+    accessor({
+      set (val: string) {
+        // must check val !== this.src here
+        // as we will set config.src in the video
+        // the may cause dead lock
+        if(this.dispatcher.readySync && this.autoload && val !== this.src) this.needToLoadSrc = true;
+        return val;
+      }
+    })
+  ],
+  autoload: alwaysBoolean(),
+  autoplay: [
+    alwaysBoolean(),
+    accessorVideoProperty('autoplay')
+  ],
+  controls: [
+    alwaysBoolean(),
+    accessorVideoProperty('controls')
+  ],
+  width: [
+    accessor({set: numberOrVoid}),
+    accessorVideoAttribute('width')
+  ],
+  height: [
+    accessor({set: numberOrVoid}),
+    accessorVideoAttribute('height')
+  ],
+  crossOrigin: [
+    accessor({set: stringOrVoid}),
+    accessorVideoAttribute({set: 'crossorigin', get: 'crossOrigin'})
+  ],
+  loop: [
+    alwaysBoolean(),
+    accessorVideoProperty('loop')
+  ],
+  defaultMuted: [
+    alwaysBoolean(),
+    accessorVideoAttribute({get: 'defaultMuted', set: 'muted', isBoolean: true})
+  ],
+  muted: [
+    alwaysBoolean(),
+    accessorVideoProperty('muted')
+  ],
+  preload: [
+    accessor({set: stringOrVoid}),
+    accessorVideoAttribute('preload')
+  ],
+  poster: [
+    accessor({set: stringOrVoid}),
+    accessorVideoAttribute('poster')
+  ],
+  playsInline: [
+    accessor({
+      get (value) {
+        const playsInline = this.videoElement.playsInline;
+        return (this.dispatcher.videoConfigReady && this.inited)
+          ? playsInline === undefined
+            ? value
+            : playsInline
+          : value;
+      },
+      set (value) {
+        if(!this.dispatcher.videoConfigReady) return value;
+        this.videoElement.playsInline = value;
+        value = value ? '' : undefined;
+        this.dispatcher.dom.setAttr('video', 'playsinline', value);
+        this.dispatcher.dom.setAttr('video', 'webkit-playsinline', value);
+        this.dispatcher.dom.setAttr('video', 'x5-video-player-type', value === '' ? 'h5' : undefined);
+        return value;
+      }
+    }),
+    alwaysBoolean()
+  ],
+  x5VideoPlayerFullScreen: [
+    accessor({set (value) {return !!value;}, get (value) {return !!value;}}),
+    accessorCustomAttribute('x5-video-player-fullscreen', true)
+  ],
+  x5VideoOrientation: [
+    accessor({set: stringOrVoid}),
+    accessorCustomAttribute('x5-video-orientation')
+  ],
+  xWebkitAirplay: [
+    accessor({set (value) {return !!value;}, get (value) {return !!value;}}),
+    accessorCustomAttribute('x-webkit-airplay', true)
+  ],
+  playbackRate: [
+    alwaysNumber(1),
+    accessorVideoProperty('playbackRate')
+  ],
+  defaultPlaybackRate: [
+    accessorVideoProperty('defaultPlaybackRate'),
+    alwaysNumber(1)
+  ],
+  disableRemotePlayback: [
+    alwaysBoolean(),
+    accessorVideoProperty('disableRemotePlayback')
+  ],
+  volume: [
+    alwaysNumber(1),
+    accessorVideoProperty('volume')
+  ]
+};
+
 export default class VideoConfig {
   dispatcher: Dispatcher;
   videoElement: HTMLVideoElement;
   inited: boolean;
 
-  @configurable
   @nonenumerable
   needToLoadSrc = false;
 
   @nonenumerable
   inited = false;
 
+  src = '';
+
+  @initString()
+  @configurable
+  type = 'vod';
+
+  @initString(str => str.toLocaleLowerCase())
+  @configurable
+  box = '';
+
+  @initArray()
+  @configurable
+  runtimeOrder = ['html5', 'flash'];
+
+  autoload = true;
+
+  autoplay = false;
+
+  controls = false;
+
+  width = undefined;
+
+  height = undefined;
+
+  crossOrigin = undefined;
+
+  loop = false;
+
+  defaultMuted = false;
+
+  muted = false;
+
+  preload = 'auto';
+
+  poster = undefined;
+
+  playsInline = false;
+
+  x5VideoPlayerFullScreen = false;
+
+  x5VideoOrientation = undefined;
+
+  xWebkitAirplay = false;
+
+  playbackRate = 1;
+
+  defaultPlaybackRate = 1;
+
+  disableRemotePlayback = false;
+
+  volume = 1;
+
+  @frozen
+  _kernelProperty = ['type', 'box', 'runtimeOrder'];
+
+  @frozen
+  _realDomAttr = ['src', 'controls', 'width', 'height', 'crossOrigin', 'loop', 'muted', 'preload', 'poster', 'autoplay', 'playsInline', 'x5VideoPlayerFullScreen', 'x5VideoOrientation', 'xWebkitAirplay', 'playbackRate', 'defaultPlaybackRate', 'autoload', 'disableRemotePlayback', 'defaultMuted', 'volume'];
+  lockKernelProperty () {
+    applyDecorators(this, {
+      type: lock,
+      box: lock,
+      runtimeOrder: lock
+    }, {self: true});
+  }
+
   constructor (dispatcher: Dispatcher, config: Object) {
+    applyDecorators(this, accessorMap, {self: true});
     Object.defineProperty(this, 'dispatcher', {
       value: dispatcher,
       enumerable: false,
@@ -102,165 +285,5 @@ export default class VideoConfig {
       this[key] = this[key];
     });
     this.inited = true;
-  }
-
-  @configurable
-  @alwaysString()
-  @accessor({
-    set (val: string) {
-      if(this.needToLoadSrc) {
-        // unlock it at first, to avoid deadlock
-        this.needToLoadSrc = false;
-        this.dispatcher.bus.emit('load', val);
-      }
-      return val;
-    }
-  }, {preSet: false})
-  @accessor({
-    set (val: string) {
-      // must check val !== this.src here
-      // as we will set config.src in the video
-      // the may cause dead lock
-      if(this.dispatcher.readySync && this.autoload && val !== this.src) this.needToLoadSrc = true;
-      return val;
-    }
-  })
-  src = '';
-
-  @initString()
-  @configurable
-  type = 'vod';
-
-  @initString(str => str.toLocaleLowerCase())
-  @configurable
-  box = '';
-
-  @initArray()
-  @configurable
-  runtimeOrder = ['html5', 'flash'];
-
-  @alwaysBoolean()
-  @configurable
-  autoload = true;
-
-  @alwaysBoolean()
-  @accessorVideoProperty('autoplay')
-  @configurable
-  autoplay = false;
-
-  @alwaysBoolean()
-  @accessorVideoProperty('controls')
-  @configurable
-  controls = false;
-
-  @accessor({set: numberOrVoid})
-  @accessorVideoAttribute('width')
-  @configurable
-  width = undefined;
-
-  @accessor({set: numberOrVoid})
-  @accessorVideoAttribute('height')
-  @configurable
-  height = undefined;
-
-  @configurable
-  @accessor({set: stringOrVoid})
-  @accessorVideoAttribute({set: 'crossorigin', get: 'crossOrigin'})
-  crossOrigin = undefined;
-
-  @alwaysBoolean()
-  @accessorVideoProperty('loop')
-  @configurable
-  loop = false;
-
-  @alwaysBoolean()
-  @accessorVideoAttribute({get: 'defaultMuted', set: 'muted', isBoolean: true})
-  @configurable
-  defaultMuted = false;
-
-  @alwaysBoolean()
-  @accessorVideoProperty('muted')
-  @configurable
-  muted = false;
-
-  @accessor({set: stringOrVoid})
-  @accessorVideoAttribute('preload')
-  @configurable
-  preload = 'auto';
-
-  @accessor({set: stringOrVoid})
-  @accessorVideoAttribute('poster')
-  @configurable
-  poster = undefined;
-
-  @configurable
-  @accessor({
-    get (value) {
-      const playsInline = this.videoElement.playsInline;
-      return (this.dispatcher.videoConfigReady && this.inited)
-        ? playsInline === undefined
-          ? value
-          : playsInline
-        : value;
-    },
-    set (value) {
-      if(!this.dispatcher.videoConfigReady) return value;
-      this.videoElement.playsInline = value;
-      value = value ? '' : undefined;
-      this.dispatcher.dom.setAttr('video', 'playsinline', value);
-      this.dispatcher.dom.setAttr('video', 'webkit-playsinline', value);
-      this.dispatcher.dom.setAttr('video', 'x5-video-player-type', value === '' ? 'h5' : undefined);
-      return value;
-    }
-  })
-  @alwaysBoolean()
-  playsInline = false;
-
-  @accessor({set (value) {return !!value;}, get (value) {return !!value;}})
-  @accessorCustomAttribute('x5-video-player-fullscreen', true)
-  @configurable
-  x5VideoPlayerFullScreen = false;
-
-  @accessor({set: stringOrVoid})
-  @accessorCustomAttribute('x5-video-orientation')
-  @configurable
-  x5VideoOrientation = undefined;
-
-  @accessor({set (value) {return !!value;}, get (value) {return !!value;}})
-  @accessorCustomAttribute('x-webkit-airplay', true)
-  @configurable
-  xWebkitAirplay = false;
-
-  @alwaysNumber(1)
-  @accessorVideoProperty('playbackRate')
-  @configurable
-  playbackRate = 1;
-
-  @accessorVideoProperty('defaultPlaybackRate')
-  @alwaysNumber(1)
-  @configurable
-  defaultPlaybackRate = 1;
-
-  @alwaysBoolean()
-  @accessorVideoProperty('disableRemotePlayback')
-  @configurable
-  disableRemotePlayback = false;
-
-  @alwaysNumber(1)
-  @accessorVideoProperty('volume')
-  @configurable
-  volume = 1;
-
-  @frozen
-  _kernelProperty = ['type', 'box', 'runtimeOrder'];
-
-  @frozen
-  _realDomAttr = ['src', 'controls', 'width', 'height', 'crossOrigin', 'loop', 'muted', 'preload', 'poster', 'autoplay', 'playsInline', 'x5VideoPlayerFullScreen', 'x5VideoOrientation', 'xWebkitAirplay', 'playbackRate', 'defaultPlaybackRate', 'autoload', 'disableRemotePlayback', 'defaultMuted', 'volume'];
-  lockKernelProperty () {
-    applyDecorators(this, {
-      type: lock,
-      box: lock,
-      runtimeOrder: lock
-    }, {self: true});
   }
 }
