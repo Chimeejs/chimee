90bb5d36da2106a485cbfd62ca78fb75b82020b2
toxic-johann <353904974@qq.com>
2017-07-29 18:51:37 +0800

    [update] finish testcase of new load what: why: how:

--- packages/chimee/__tests__/silentload.js
+++ packages/chimee/__tests__/silentload.js
@@ -1,4 +1,5 @@
 import Chimee from 'index';
+import ChimeeKernelHls from 'chimee-kernel-hls';
 import {bind, Log} from 'chimee-helper';
 describe('$silentLoad', () => {
   let player;
@@ -185,5 +186,68 @@ describe('$silentLoad', () => {
     expect(player.__dispatcher.dom.videoElement).toBe(video);
     expect(player.src).toBe('http://cdn.toxicjohann.com/%E4%BA%8E%E6%98%AF.mp4');
     expect(Log.data.error).toEqual([["chimee's silentload", 'MEDIA_ELEMENT_ERROR: Format error']]);
+  });
+});
+
+describe('load', () => {
+  let player;
+  let oldVideo;
+  let oldKernel;
+  let video;
+  let originFn;
+  beforeEach(() => {
+    player = new Chimee({
+      wrapper: document.createElement('div'),
+      src: 'http://cdn.toxicjohann.com/lostStar.mp4'
+    });
+    oldVideo = player.__dispatcher.dom.videoElement;
+    oldKernel = player.__dispatcher.kernel;
+    Log.data.warn = [];
+    Log.data.error = [];
+    originFn = global.document.createElement;
+    global.document.createElement = function (tag) {
+      if(tag === 'video') {
+        video = bind(originFn, document)(tag);
+        return video;
+      }
+      return bind(originFn, document)(tag);
+    };
+  });
+  afterEach(() => {
+    global.document.createElement = originFn;
+    player.destroy();
+  });
+  test('load with different box', async () => {
+    player.load('http://yunxianchang.live.ujne7.com/vod-system-bj/79_3041054cc65-ae8c-4b63-8937-5ccb05f79720.m3u8', {
+      box: 'hls',
+      preset: {
+        hls: ChimeeKernelHls
+      }
+    });
+    await Promise.resolve();
+    expect(player.$video).not.toBe(oldVideo);
+    expect(player.$video).toBe(video);
+    expect(player.__dispatcher.kernel).not.toBe(oldKernel);
+  });
+  test('load with different preset', async () => {
+    player.load('http://cdn.toxicjohann.com/%E4%BA%8E%E6%98%AF.mp4', {
+      preset: {
+        hls: ChimeeKernelHls
+      }
+    });
+    await Promise.resolve();
+    expect(player.$video).not.toBe(oldVideo);
+    expect(player.$video).toBe(video);
+    expect(player.__dispatcher.kernel).not.toBe(oldKernel);
+  });
+  test('load with same box', async () => {
+    player.load('http://cdn.toxicjohann.com/%E4%BA%8E%E6%98%AF.mp4', {
+      isLive: false,
+      box: 'native'
+    });
+    await Promise.resolve();
+    expect(player.$video).not.toBe(oldVideo);
+    expect(player.$video).toBe(video);
+    expect(player.__dispatcher.kernel).not.toBe(oldKernel);
   });
 });
