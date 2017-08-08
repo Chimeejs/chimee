2e35b0a5e9f7d5f4553fe17c0f7d44c5eb24ada0
toxic-johann <353904974@qq.com>
2017-08-08 21:00:08 +0800

    [update] fix bug on dev and umd what: why: how:

--- packages/chimee/build/rollup.config.umd.js
+++ packages/chimee/build/rollup.config.umd.js
@@ -1,7 +1,12 @@
 import base from './rollup.config.base';
 const {name} = require('../package.json');
 import {camelize} from 'toxic-utils';
-export default Object.assign(base('umd'), {
+import replace from 'rollup-plugin-replace';
+const config = base('umd');
+config.plugins.unshift(replace({
+  'process.env.NODE_ENV': '"development"'
+}));
+export default Object.assign(config, {
   format: 'umd',
   dest: 'lib/index.browser.js',
   moduleName: camelize(name, true)
