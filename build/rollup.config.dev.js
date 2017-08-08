e504aaafd1325f536c806335a4d61934332d4bb8
toxic-johann <353904974@qq.com>
2017-08-08 21:00:08 +0800

    [update] fix bug on dev and umd what: why: how:

--- build/rollup.config.dev.js
+++ build/rollup.config.dev.js
@@ -1,6 +1,7 @@
 import base from './rollup.config.base';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
+import replace from 'rollup-plugin-replace';
 import {camelize} from 'toxic-utils';
 const {name} = require('../package.json');
 const config = base('iife');
@@ -8,6 +9,9 @@ config.plugins.push(
   serve(),
   livereload()
 );
+config.plugins.unshift(replace({
+  'process.env.NODE_ENV': '"development"'
+}));
 export default Object.assign(config, {
   format: 'umd',
   dest: 'lib/index.dev.js',
