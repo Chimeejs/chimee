111b5c7e0642da060e24478d3a10dca91006e8ed
toxic-johann <toxic.johann@gmail.com>
2017-08-14 17:44:54 +0800

    [update] add IE devlopment case What: Why: How:

---
+++ packages/chimee/demo/ie/rollup.config.js
@@ -0,0 +1,21 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+export default {
+  entry: 'demo/ie/index.js',
+  format: 'umd',
+  dest: 'demo/ie/dist.js',
+  plugins: [
+    babel({
+      presets: ['es2015-rollup', 'stage-0'],
+      exclude: 'node_modules/**',
+      babelrc: false
+    }),
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: ['src', 'node_modules']
+      }
+    }),
+    commonjs()
+  ]
+};
