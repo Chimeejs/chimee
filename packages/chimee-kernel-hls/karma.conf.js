e25237bb09d6ad0d6c066abb2920c4e95c04ee8b
toxic-johann <353904974@qq.com>
2017-12-23 20:04:59 +0800

    [update] use firefox headless

--- packages/chimee-kernel-hls/karma.conf.js
+++ packages/chimee-kernel-hls/karma.conf.js
@@ -96,7 +96,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ 'Firefox' ],
+    browsers: [ 'FirefoxHeadless' ],
+
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: [ '-headless' ],
+      },
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
