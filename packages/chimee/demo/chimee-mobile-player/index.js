30e5e6bd9d98eb395b8f2029b6aa15609e4e3908
toxic-johann <toxic.johann@gmail.com>
2018-02-11 19:16:27 +0800

    [update] update chimee-mobile-player close https://github.com/Chimeejs/chimee/issues/91 close https://github.com/Chimeejs/chimee/issues/88

--- packages/chimee/demo/chimee-mobile-player/index.js
+++ packages/chimee/demo/chimee-mobile-player/index.js
@@ -1,12 +1,12 @@
-new window.ChimeeMobilePlayer({
+window.player = new window.ChimeeMobilePlayer({
   wrapper: '#wrapper',
   src: 'http://cdn.toxicjohann.com/lostStar.mp4',
   autoplay: true,
   controls: true,
   playsInline: true,
   preload: true,
-  x5VideoPlayerFullscreen: true,
-  x5VideoOrientation: true,
+  // x5VideoPlayerFullscreen: true,
+  // x5VideoOrientation: true,
   xWebkitAirplay: true,
   muted: true,
 });
