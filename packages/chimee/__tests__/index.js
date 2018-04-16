933ed7e30ae48a70df8bad2eceb4203f457c7662
toxic-johann <353904974@qq.com>
2018-04-16 21:51:53 +0800

    [fix] split __tests__/index.js into several test file

--- packages/chimee/__tests__/index.js
+++ packages/chimee/__tests__/index.js
@@ -1,7 +1,5 @@
 import Chimee from 'index';
-import { Log, getAttr, setAttr, bind } from 'chimee-helper';
-import { videoReadOnlyProperties } from 'helper/const';
-// import chimeeKernelFlv from 'chimee-kernel-flv';
+import { Log, bind } from 'chimee-helper';
 
 describe('Chimee', () => {
   let player;
@@ -37,97 +35,6 @@ describe('Chimee', () => {
   test('focus', () => {
     const player = new Chimee(document.createElement('div'));
     expect(() => player.focus()).not.toThrow();
-  });
-  test('stop fullscreen', () => {
-    const plugin = {
-      name: 'stopFullscreen',
-      events: {
-        beforeFullscreen() {
-          return false;
-        },
-      },
-    };
-    Chimee.install(plugin);
-    const player = new Chimee({
-      wrapper: document.createElement('div'),
-      plugin: [ 'stopFullscreen' ],
-    });
-    expect(player.fullscreen()).toBe(false);
-  });
-});
-
-describe('isFullscreen and fullscreenElement', () => {
-  let player;
-  let wrapper;
-  let originURLrevoke;
-  beforeEach(() => {
-    wrapper = document.createElement('div');
-    document.body.appendChild(wrapper);
-    player = new Chimee(wrapper);
-    originURLrevoke = global.URL.revokeObjectURL;
-    global.URL.revokeObjectURL = () => {};
-  });
-  afterEach(() => {
-    global.URL.revokeObjectURL = originURLrevoke;
-    player.exitFullscreen();
-    wrapper.parentNode.removeChild(wrapper);
-    player.destroy();
-  });
-  // test('default to be false', () => {
-  //   expect(player.isFullscreen).toBe(false);
-  //   expect(player.fullscreenElement).toBe();
-  // });
-  test('wrapper', () => {
-    const target = player.__dispatcher.dom.wrapper;
-    const fn1 = jest.fn();
-    player.$watch('isFullscreen', fn1);
-    const fn2 = jest.fn();
-    player.$watch('fullscreenElement', fn2);
-    player.requestFullscreen('wrapper');
-    expect(player.isFullscreen).toBe(true);
-    expect(player.__dispatcher.dom[player.fullscreenElement]).toBe(target);
-    expect(fn1).toHaveBeenCalledTimes(1);
-    expect(fn2).toHaveBeenCalledTimes(1);
-    expect(fn1).lastCalledWith(true, false);
-    expect(fn2).lastCalledWith('wrapper', undefined);
-  });
-  test('container', () => {
-    const target = player.__dispatcher.dom.container;
-    player.requestFullscreen('container');
-    expect(player.isFullscreen).toBe(true);
-    expect(player.__dispatcher.dom[player.fullscreenElement]).toBe(target);
-  });
-  test('video', () => {
-    const target = player.__dispatcher.dom.videoElement;
-    player.requestFullscreen('video');
-    expect(player.isFullscreen).toBe(true);
-    expect(player.__dispatcher.dom[player.fullscreenElement + 'Element']).toBe(target);
-  });
-  // test('plugin', () => {
-  //   const wrapper = document.createElement('div');
-  //   const player = new Chimee({
-  //     wrapper,
-  //     plugin: [ 'stopFullscreen' ],
-  //   });
-  //   document.body.appendChild(wrapper);
-  //   const target = player.stopFullscreen.$dom;
-  //   esFullscreen.open(target);
-  //   expect(player.isFullscreen).toBe(true);
-  //   expect(player.fullscreenElement).toBe(target);
-  //   document.body.removeChild(wrapper);
-  // });
-
-  test('fullscreen event and fullscreenchange event', () => {
-    const fn = jest.fn();
-    const changeFn = jest.fn();
-    player.on('fullscreen', fn);
-    player.on('fullscreenchange', changeFn);
-    player.fullscreen();
-    expect(fn).toHaveBeenCalledTimes(1);
-    expect(changeFn).toHaveBeenCalledTimes(1);
-    player.fullscreen(false);
-    expect(fn).toHaveBeenCalledTimes(2);
-    expect(changeFn).toHaveBeenCalledTimes(2);
   });
 
   describe('$video, $container and $wrapper', () => {
