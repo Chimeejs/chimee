5d608ae8b769289f0a59e137de99649e96232dc0
toxic-johann <353904974@qq.com>
2018-04-16 21:51:53 +0800

    [fix] split __tests__/index.js into several test file

--- __tests__/chimee/fullscreen.js
+++ __tests__/chimee/fullscreen.js
@@ -1,6 +1,7 @@
 import Chimee from 'index';
+import esFullscreen from 'es-fullscreen';
 
-describe('fullscreen', () => {
+describe('chimee fullscreen', () => {
   let player;
   let wrapper;
   let originURLrevoke;
@@ -91,5 +92,53 @@ describe('fullscreen', () => {
       plugin: [ 'stopFullscreen' ],
     });
     expect(player.fullscreen()).toBe(false);
+  });
+
+  test('default to be false', () => {
+    expect(player.isFullscreen).toBe(false);
+    expect(player.fullscreenElement).toBe();
+  });
+
+  test('wrapper', () => {
+    const target = player.__dispatcher.dom.wrapper;
+    const fn1 = jest.fn();
+    player.$watch('isFullscreen', fn1);
+    const fn2 = jest.fn();
+    player.$watch('fullscreenElement', fn2);
+    player.requestFullscreen('wrapper');
+    expect(player.isFullscreen).toBe(true);
+    expect(player.__dispatcher.dom[player.fullscreenElement]).toBe(target);
+    expect(fn1).toHaveBeenCalledTimes(1);
+    expect(fn2).toHaveBeenCalledTimes(1);
+    expect(fn1).lastCalledWith(true, false);
+    expect(fn2).lastCalledWith('wrapper', undefined);
+  });
+
+  test('container', () => {
+    const target = player.__dispatcher.dom.container;
+    player.requestFullscreen('container');
+    expect(player.isFullscreen).toBe(true);
+    expect(player.__dispatcher.dom[player.fullscreenElement]).toBe(target);
+  });
+
+  test('video', () => {
+    const target = player.__dispatcher.dom.videoElement;
+    player.requestFullscreen('video');
+    expect(player.isFullscreen).toBe(true);
+    expect(player.__dispatcher.dom[player.fullscreenElement + 'Element']).toBe(target);
+  });
+
+  test('plugin', () => {
+    const wrapper = document.createElement('div');
+    const player = new Chimee({
+      wrapper,
+      plugin: [ 'stopFullscreen' ],
+    });
+    document.body.appendChild(wrapper);
+    const target = player.stopFullscreen.$dom;
+    esFullscreen.open(target);
+    expect(player.isFullscreen).toBe(true);
+    expect(player.fullscreenElement).toBe(target);
+    document.body.removeChild(wrapper);
   });
 });
