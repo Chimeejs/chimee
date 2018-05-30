8a18fb50906b3fd8ef85e9bddd15ed2ee3ccec8e
toxic-johann <353904974@qq.com>
2018-05-30 23:30:51 +0800

    fix(binder): should bind on penetrate node whatevent plugin want to listen to event
    related to https://github.com/Chimeejs/chimee/issues/128

--- demo/chimee-plugin-controlbar/index.js
+++ demo/chimee-plugin-controlbar/index.js
@@ -1,21 +1,26 @@
 const Chimee = window.Chimee;
 const chimeePluginControlbar = window.chimeePluginControlbar;
+const myPlugin = {
+  name: 'my-plugin',
+  penetrate: true,
+  el: '<div class="my-plugin"></div>',
+  create() {},
+  events: {
+    // click(evt) {
+    //   console.warn(evt);
+    // },
+  },
+};
 Chimee.install(chimeePluginControlbar);
+Chimee.install(myPlugin);
 const player = new Chimee({
   // 播放地址
-  // src: 'http://cdn.toxicjohann.com/lostStar.mp4',
   src: 'http://cdn.toxicjohann.com/%E4%BA%8E%E6%98%AF.mp4',
-  // src: 'http://yunxianchang.live.ujne7.com/vod-system-bj/TL1ce1196bce348070bfeef2116efbdea6.flv',
-  // src: 'http://yunxianchang.live.ujne7.com/vod-system-bj/79_3041054cc65-ae8c-4b63-8937-5ccb05f79720.m3u8',
-  // dom容器
   wrapper: '#wrapper',
-  plugin: [ chimeePluginControlbar.name ],
-  // preset: {
-  //   flv: window.chimeeKernelFlv,
-  //   hls: window.chimeeKernelHls
-  // },
-  // width: 60,
-  // height: 90,
+  plugin: [
+    chimeePluginControlbar.name,
+    myPlugin.name,
+  ],
   autoplay: true,
   controls: true,
   muted: true,
