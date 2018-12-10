f53053a0fb4e54f6a7a5889596a2c233939dc554
toxic-johann <353904974@qq.com>
2018-12-10 22:24:13 +0800

    test: update unit test for helper/utils

--- __tests__/helper/utils.js
+++ __tests__/helper/utils.js
@@ -4,37 +4,37 @@ describe('runRejectableQueue', async () => {
     expect(runRejectableQueue([])).resolves.toBe();
   });
   test('no function', () => {
-    expect(runRejectableQueue([1, 2, 3])).resolves.toBe();
+    expect(runRejectableQueue([ 1, 2, 3 ])).resolves.toBe();
   });
   test('no function but has a false', () => {
-    expect(runRejectableQueue([1, 2, false, 3])).rejects.toMatch('stop');
+    expect(runRejectableQueue([ 1, 2, false, 3 ])).rejects.toMatch('stop');
   });
   test('functions', () => {
     expect(runRejectableQueue([
       () => {},
       () => {},
-      () => {}
+      () => {},
     ])).resolves.toBe();
   });
   test('functions and promise', () => {
     expect(runRejectableQueue([
       () => {},
       Promise.resolve(),
-      () => new Promise(resolve => resolve())
+      () => new Promise(resolve => resolve()),
     ])).resolves.toBe();
   });
   test('function and promise.reject', () => {
     expect(runRejectableQueue([
       () => {},
       Promise.reject(),
-      () => new Promise(resolve => resolve())
+      () => new Promise(resolve => resolve()),
     ])).rejects.toMatch('stop');
   });
   test('function return promise.reject', () => {
     expect(runRejectableQueue([
       () => {},
       Promise.resolve(),
-      () => new Promise((resolve, reject) => reject())
+      () => new Promise((resolve, reject) => reject()),
     ])).rejects.toMatch('stop');
   });
   test('order', async () => {
@@ -46,21 +46,21 @@ describe('runRejectableQueue', async () => {
       () => checkArray.push(3),
       () => checkArray.push(4),
     ])).rejects.toMatch('stop');
-    expect(checkArray).toEqual([1, 2]);
+    expect(checkArray).toEqual([ 1, 2 ]);
   });
   test('error catch', async () => {
     const error = new Error('i am an error');
     await expect(runRejectableQueue([
       () => {},
       () => {},
-      () => {throw error;}
+      () => { throw error; },
     ])).rejects.toBe(error);
   });
   test('reject info catch', async () => {
-     await expect(runRejectableQueue([
+    await expect(runRejectableQueue([
       () => {},
       () => Promise.reject('abc'),
-      () => {}
+      () => {},
     ])).rejects.toBe('abc');
   });
 });
