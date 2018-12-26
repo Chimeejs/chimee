3b2d4dfa4235f8fa1e937603115ddb7eef6c7217
toxic-johann <353904974@qq.com>
2018-12-27 00:31:20 +0800

    style: type should be upper case

--- packages/chimee/src/types/base.ts
+++ packages/chimee/src/types/base.ts
@@ -1,4 +1,68 @@
-export type eventStage = 'before' | 'main' | 'after' | '_';
+import { IVideoKernelConstructor } from 'kernels/base';
 
-export type binderTarget = 'kernel' | 'container' | 'wrapper' | 'video' | 'video-dom' | 'plugin' | 'esFullscreen';
+export type EventStage = 'before' | 'main' | 'after' | '_';
 
+export type BinderTarget = 'kernel' | 'container' | 'wrapper' | 'video' | 'video-dom' | 'plugin' | 'esFullscreen';
+
+export type PluginOption = {
+  alias?: string,
+  level?: number,
+  name: string,
+  operable?: boolean,
+};
+
+export type VesselConfig = {
+  height?: number | string,
+  width?: number | string,
+};
+
+export type SingleKernelConfig = {
+  [key: string]: any,
+  handler?: IVideoKernelConstructor | string,
+  name?: string,
+};
+
+export type UserKernelsConfig = string[] | {
+  flv?: IVideoKernelConstructor,
+  hls?: IVideoKernelConstructor,
+  mp4?: IVideoKernelConstructor,
+} | {
+  flv?: SingleKernelConfig,
+  hls?: SingleKernelConfig,
+  mp4?: SingleKernelConfig,
+} | SingleKernelConfig[];
+
+export type UserConfig = {
+  autoload?: boolean,
+  autoplay?: boolean,
+  box?: string,
+  container?: VesselConfig,
+  controls?: boolean,
+  crossorigin?: string,
+  defaultMuted?: boolean,
+  defaultPlaybackRate?: number,
+  disableRemotePlayback?: boolean,
+  height?: number,
+  isLive?: boolean,
+  kernels?: UserKernelsConfig,
+  loop?: boolean,
+  muted?: boolean,
+  noDefaultContextMenu?: string | boolean,
+  playbackRate?: number,
+  playsinline?: boolean,
+  plugin?: Array<string | PluginOption>,
+  poster?: string,
+  preload?: string,
+  preset?: {
+    flv?: IVideoKernelConstructor,
+    hls?: IVideoKernelConstructor,
+  },
+  runtimeOrder?: string[],
+  src?: string,
+  videoRequiredGuardedAttributes?: string[],
+  width?: number,
+  wrapper: string | Element,
+  x5VideoOrientation?: string,
+  x5VideoPlayerFullScreen?: boolean,
+  xWebkitAirplay?: boolean,
+};
