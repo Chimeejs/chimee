126f2db401c9d9fc0493602f8ad9d51889fb3a3f
toxic-johann <353904974@qq.com>
2019-01-19 10:08:44 +0800

    test: finish binder unit test

--- __tests__/dispatcher/binder.js
+++ __tests__/dispatcher/binder.js
@@ -33,58 +33,58 @@ describe('dispatcher/binder', () => {
     expect(() => player.destroy()).not.toThrow();
   });
 
-  // test('binder will clear video events which is forget to clear by upper layer', () => {
-  //   player.dispatcher.binder.on({
-  //     target: 'video',
-  //     name: 'play',
-  //     id: 'illegal',
-  //     fn: () => {},
-  //   });
-  //   expect(() => player.destroy()).not.toThrow();
-  // });
+  test('binder will clear video events which is forget to clear by upper layer', () => {
+    player.dispatcher.binder.on({
+      target: 'video',
+      name: 'play',
+      id: 'illegal',
+      fn: () => {},
+    });
+    expect(() => player.destroy()).not.toThrow();
+  });
 
-  // test('binder triggersync isEventEmitalbe backup', () => {
-  //   expect(() => player.dispatcher.binder.triggerSync({})).not.toThrow();
-  // });
+  test('binder triggersync isEventEmitalbe backup', () => {
+    expect(() => player.dispatcher.binder.triggerSync({})).not.toThrow();
+  });
 
-  // test('binder emitsync isEventEmitalbe backup', () => {
-  //   expect(() => player.dispatcher.binder.emitSync({ name: 'hello' })).not.toThrow();
-  // });
+  test('binder emitsync isEventEmitalbe backup', () => {
+    expect(() => player.dispatcher.binder.emitSync({ name: 'hello' })).not.toThrow();
+  });
 
-  // test('prettifyEventParameter without function', () => {
-  //   expect(() => {
-  //     player.dispatcher.binder.on({
-  //       name: 'what',
-  //       target: 'plugin',
-  //       stage: 'main',
-  //     });
-  //   }).toThrow('You must provide a function to handle with event what, but not undefined');
-  // });
+  test('prettifyEventParameter without function', () => {
+    expect(() => {
+      player.dispatcher.binder.on({
+        name: 'what',
+        target: 'plugin',
+        stage: 'main',
+      });
+    }).toThrow('You must provide a function to handle with event what, but not undefined');
+  });
 
-  // test('off redudant event which has no function bind', () => {
-  //   player.dispatcher.binder.bindedEventNames.kernel.push('heartbeat');
-  //   player.on('mediaInfo', () => {});
-  //   player.off('heartbeat', () => {});
-  // });
+  test('off redudant event which has no function bind', () => {
+    player.dispatcher.binder.bindedEventNames.kernel.push('heartbeat');
+    player.on('mediaInfo', () => {});
+    player.off('heartbeat', () => {});
+  });
 
-  // test('off redudant event which has no name index', () => {
-  //   player.off('heartbeat', () => {});
-  // });
+  test('off redudant event which has no name index', () => {
+    player.off('heartbeat', () => {});
+  });
 
-  // test('penetrate event binding', async () => {
-  //   const plugin = {
-  //     name: 'penetrate plugin',
-  //     penetrate: true,
-  //     events: {
-  //       click() {},
-  //     },
-  //   };
-  //   Chimee.install(plugin);
-  //   await player.use(plugin.name);
-  // });
+  test('penetrate event binding', async () => {
+    const plugin = {
+      name: 'penetrate plugin',
+      penetrate: true,
+      events: {
+        click() {},
+      },
+    };
+    Chimee.install(plugin);
+    await player.use(plugin.name);
+  });
 
-  // test('bind the same event', async () => {
-  //   player.on('click', () => {});
-  //   player.on('click', () => {});
-  // });
+  test('bind the same event', async () => {
+    player.on('click', () => {});
+    player.on('click', () => {});
+  });
 });
